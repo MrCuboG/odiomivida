012ee24a87812548a94bf85fd72f0343f62343ed
Luis Antonio Arguello Escutia <luisantoniobros10@gmail.com>
2024-05-20 07:58:21 -0600

    diferencia

--- prueba/src/Main.java
+++ prueba/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Diferencia");
         System.out.println("Otra diferencia");
+        System.out.println("pedo");
     }
 }
